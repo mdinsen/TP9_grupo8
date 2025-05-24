d13c35bb37a6db78aba8993a2aabe6a19608fa11
mdinsen <mdinsen@itba.edu.ar>
2025-05-23 23:27:27 -0300

    no admite mas de 2 caracteres

--- leds.c
+++ leds.c
@@ -9,7 +9,7 @@ int main(void){
     printf("Ingrese el numero de LED que desee modificar,\n't' para cambiar de estado los LEDs al estado opuesto,\n'c' para apagar todos,\n's' para encender todos,\n y 'q' para salir.\n");
     char c = getchar();
     int n = -1;
-    
+    int valido = 1;                             //Flag para cuando se ingresan mas de un caracter
 
 
     while(c != 'q'){                          //Al presionar 'q' se sale del programa
@@ -20,25 +20,30 @@ int main(void){
             while (siguiente != '\n' && siguiente != EOF){
                 siguiente = getchar();
             }
+            valido = 0;
+        }else{
+            valido = 1;
         }
 
         if(esNumero(c)){
             n = ingresarNumero(c);
             bitset(A, n);                  //Encendemos el bit "n" del puerto A que ingresó el usuario, en caso de que haya ingresado un numero valido: del 0 al 7
         }else{
-            switch(c){
-                case 't':
-                    masktoggle(A, 0xFF);   //Con 't' cambiamos el estado de todos los bits del puerto A: si eran 0s pasan a ser 1s y viceversa
-                    break;
-                case 'c':
-                    maskoff(A, 0xFF);      //Con 'c' se "apagan" todos los bits: pasan a ser todos 0
-                    break;
-                case 's':
-                    maskon(A, 0xFF);       //Con 's' se "encienden" todos los bits: pasan a ser todos 1
-                    break;
-                default:
-                    printf("Por favor ingrese una opcion valida\n");     //en caso de ingresar alguna opcion que no sea valida, se imprime el estado anterior de los bits y se le pide nuevamente al usuario que ingrese otra opcion
-                    break;
+            if(valido){
+                switch(c){
+                    case 't':
+                        masktoggle(A, 0xFF);   //Con 't' cambiamos el estado de todos los bits del puerto A: si eran 0s pasan a ser 1s y viceversa
+                        break;
+                    case 'c':
+                        maskoff(A, 0xFF);      //Con 'c' se "apagan" todos los bits: pasan a ser todos 0
+                        break;
+                    case 's':
+                        maskon(A, 0xFF);       //Con 's' se "encienden" todos los bits: pasan a ser todos 1
+                        break;
+                    default:
+                        printf("Por favor ingrese una opcion valida\n");     //en caso de ingresar alguna opcion que no sea valida, se imprime el estado anterior de los bits y se le pide nuevamente al usuario que ingrese otra opcion
+                        break;
+                }
             }
         }
         print_bits(A);
